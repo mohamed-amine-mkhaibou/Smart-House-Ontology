e2826c886b6d5e99799bd025652650fc2c79005d
wjy862 <junyi.wu002@gmail.com>
2022-03-07 18:35:51 +0100

    refactoring la fonction createInstance

--- src/itsudparis/application/Main.java
+++ src/itsudparis/application/Main.java
@@ -7,76 +7,23 @@ import itsudparis.tools.JenaEngine;
 
 public class Main {
 
+
 	public static void main(String[] args) {
-		
+		//définir les parametre
 	    String inputDataOntology = "data/SmartHome_test.owl";
 	    final String ns = "http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#";
 	    final String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
 	    final String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 	    String inputRule = "data/owlrules.txt";
-
+ 		//créer le model
 	    Model model = JenaEngine.readModel(inputDataOntology);
-
 		Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, inputRule);
+		//créer les Instances
+		createInstance(inferedModel,ns);
+		//lire les streams
+		StreamInputDataset myStream = new StreamInputDataset();
+		myStream.run(inferedModel);
 
-       
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "table_dining");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "table_dining", "estDans", "Dining");
-
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "bed");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "bed", "estDans", "Bedroom_big");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "bureau");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "bureau", "estDans", "Office");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "refrigerateur");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "refrigerateur", "estDans", "Kitchen");
-        
-        //JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "table_dining");
-        //JenaEngine.addValueOfObjectProperty(inferedModel, ns, "table_dining", "estDans", "Dining");
-        
- 
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Volet", "volet_living");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "volet_living", "estDans", "Living");
-
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Volet", "volet_kitchen");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "volet_kitchen", "estDans", "Kitchen");
-       // JenaEngine.updateValueOfDataTypeProperty(inferedModel, ns, "volet_kitchen", "etat", "fermee");
-
-        //JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "refrigerateur");
-        //JenaEngine.addValueOfObjectProperty(inferedModel, ns, "refrigerateur", "estDans", "Kitchen");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "Cuisiniere");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "Cuisiniere", "estDans", "Kitchen");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "Micro-ondes");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "Micro-ondes", "estDans", "Kitchen");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "Table-a-manger");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "Table-a-manger", "estDans", "Kitchen");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Objet", "Chaise");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "Chaise", "estDans", "Kitchen");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Volet", "volet_kitchen");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "volet_kitchen", "estDans", "Kitchen");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Chauffage", "chauffage_office");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "chauffage_office", "estDans", "Office");
-        JenaEngine.updateValueOfDataTypeProperty(inferedModel, ns, "chauffage_office", "etat", "OFF");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"Lampe", "lampe_living");
-        JenaEngine.updateValueOfDataTypeProperty(inferedModel, ns, "lampe_living", "etat", "allumee");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "lampe_living", "estDans", "Living");
-        
-        JenaEngine.createInstanceOfClass(inferedModel, ns,"TV", "tv_living");
-        JenaEngine.updateValueOfDataTypeProperty(inferedModel, ns, "tv_living", "etat", "allumee");
-        JenaEngine.addValueOfObjectProperty(inferedModel, ns, "tv_living", "estDans", "Living");
-        	
-		StreamInputDataset stream = new StreamInputDataset();
-		stream.run(inferedModel);
-		
 		System.out.println("Les capteurs dans Kitchen:");
 		System.out.println(JenaEngine.executeQuery(inferedModel, "SELECT ?capteur WHERE { ?capteur <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"
 				+ " <http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#Mouvement> ."
@@ -133,4 +80,57 @@ public class Main {
 		
 	}
 
+	public static void createInstance(Model inferedModel,String owl){
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "table_dining");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "table_dining", "estDans", "Dining");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "bed");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "bed", "estDans", "Bedroom_big");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "bureau");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "bureau", "estDans", "Office");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "refrigerateur");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "refrigerateur", "estDans", "Kitchen");
+
+		//JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "table_dining");
+		//JenaEngine.addValueOfObjectProperty(inferedModel, owl, "table_dining", "estDans", "Dining");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Volet", "volet_living");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "volet_living", "estDans", "Living");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Volet", "volet_kitchen");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "volet_kitchen", "estDans", "Kitchen");
+		// JenaEngine.updateValueOfDataTypeProperty(inferedModel, owl, "volet_kitchen", "etat", "fermee");
+
+		//JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "refrigerateur");
+		//JenaEngine.addValueOfObjectProperty(inferedModel, owl, "refrigerateur", "estDans", "Kitchen");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "Cuisiniere");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "Cuisiniere", "estDans", "Kitchen");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "Micro-ondes");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "Micro-ondes", "estDans", "Kitchen");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "Table-a-manger");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "Table-a-manger", "estDans", "Kitchen");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Objet", "Chaise");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "Chaise", "estDans", "Kitchen");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Volet", "volet_kitchen");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "volet_kitchen", "estDans", "Kitchen");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Chauffage", "chauffage_office");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "chauffage_office", "estDans", "Office");
+		JenaEngine.updateValueOfDataTypeProperty(inferedModel, owl, "chauffage_office", "etat", "OFF");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"Lampe", "lampe_living");
+		JenaEngine.updateValueOfDataTypeProperty(inferedModel, owl, "lampe_living", "etat", "allumee");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "lampe_living", "estDans", "Living");
+
+		JenaEngine.createInstanceOfClass(inferedModel, owl,"TV", "tv_living");
+		JenaEngine.updateValueOfDataTypeProperty(inferedModel, owl, "tv_living", "etat", "allumee");
+		JenaEngine.addValueOfObjectProperty(inferedModel, owl, "tv_living", "estDans", "Living");
+	}
 }
