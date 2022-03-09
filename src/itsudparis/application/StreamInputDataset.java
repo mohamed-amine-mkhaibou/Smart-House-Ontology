9eb21d08ad109a07e7e37832df71adca9554a01e
wjy862 <junyi.wu002@gmail.com>
2022-03-09 18:10:49 +0100

    Améliorer les prints

--- src/itsudparis/application/StreamInputDataset.java
+++ src/itsudparis/application/StreamInputDataset.java
@@ -2,6 +2,8 @@ package itsudparis.application;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
@@ -320,95 +322,158 @@ public class StreamInputDataset {
         }
 
     }
-
+    public static boolean equalToOneCapteursInList(Resource subject,String [] listCapteur){
+        boolean flag=false;
+        String prefix="http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#";
+        for(String capteur:listCapteur){
+            flag=flag||subject.toString().equalsIgnoreCase(prefix+capteur);
+        }
+        return flag;
+    }
     public static void detectAction(StmtIterator geriter, Model model) throws InterruptedException {
-        int bool = 0;
+        boolean frontDoor=false,corridor=false,Bathroom=false,table=false,kitchen=false,livingRoom=false,hostBedroom=false,guestBedroom=false,wardrobe=false,Office=false;
+        String[] frontDoorCapteurs=new String[]{"D001"};
+        String[] BathroomCapteurs=new String[]{"M031"};
+        String[] tableCapteurs=new String[]{"M014"};
+        String[] kitchenCapteurs=new String[]{"M015","M016","M017","M018","M019"};
+        String[] livingRoomCapteurs=new String[]{"M009","M010","M011","M012","M013","M020"};
+        String[] hostBedroomCapteurs=new String[]{"M001","M002","M003","M004","M005","M006","M007"};
+        String[] guestBedroomCapteurs=new String[]{"M024"};
+        String[] officeCapteurs=new String[]{"M025","M026","M027"};
+        String[] wardrobeCapteurs=new String[]{"M031"};
+        String[] corridorCapteurs=new String[]{"M021","M022"};
+        //Map<String[],boolean> capteurs=new String[][]{frontDoorCapteurs,BathroomCapteurs,tableCapteurs,kitchenCapteurs,livingRoomCapteurs,hostBedroomCapteurs,guestBedroomCapteurs,officeCapteurs,wardrobeCapteurs,corridorCapteurs};
+        Map<String[],Boolean> capteurs=new HashMap<>();
+        capteurs.put(frontDoorCapteurs,false);
+        capteurs.put(BathroomCapteurs,false);
+        capteurs.put(tableCapteurs,false);
+        capteurs.put(kitchenCapteurs,false);
+        capteurs.put(livingRoomCapteurs,false);
+        capteurs.put(hostBedroomCapteurs,false);
+        capteurs.put(guestBedroomCapteurs,false);
+        capteurs.put(officeCapteurs,false);
+        capteurs.put(wardrobeCapteurs,false);
+        capteurs.put(corridorCapteurs,false);
+
+
         while (geriter.hasNext()) {
             Statement stmt = geriter.nextStatement();
             Resource subject = stmt.getSubject();
             Property predicate = stmt.getPredicate();
             RDFNode object = stmt.getObject();
             if (!object.toString().startsWith("http")) {
-                boolean one = false;
-                boolean two = false;
-                boolean three = false;
-                boolean four = false;
-                //Capteur porte Bathroom D003
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#D003")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        one = true;
-                    }
-                }
-                //M31 capteur de mouvement dans BATHROOM
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M031")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        two = true;
-                    }
-                }
-                if (two) {
-                    TimeUnit.SECONDS.sleep(0);
-                    System.out.print("-----------someone is using the Bathroom-----------");
-
-                }
-                //M0014 SALLE
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M014")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        TimeUnit.SECONDS.sleep(0);
-
-                        System.out.print("--------- Someone is behind the table-----------");
-                    }
-                }
-                //M19 KITCHEN
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M019")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        three = true;
-                    }
-                }
-                //M0015 KITCHEN
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M015")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        four = true;
-                    }
-                }
-                if (three && four) {
-                    TimeUnit.SECONDS.sleep(0);
-
-                    System.out.print("--------Someone is cooking in the kitchen--------");
-                }
-                //M020 living
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M020")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        System.out.print("--------Someone is at living room--------");
-                    }
-                }
-                //M007 host bedroom
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M007")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        System.out.print("--------Someone is at host bedroom--------");
-                    }
-                }
-                //M024 guest bedroom
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M024")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        System.out.print("--------Someone is at guest bedroom--------");
-                    }
-                }
-                //M027 Office
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M027")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        System.out.print("--------someone is in front of the wardrobe--------");
+                //pour chaque groupe de capteur détecte si un capteur entre eux est activé
+                for (Map.Entry<String[],Boolean> entry : capteurs.entrySet()) {
+                    String[] key=entry.getKey();
+                    if (equalToOneCapteursInList(subject,key)){
+                        if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+                            capteurs.put(key,true);
+                        }
                     }
                 }
 
-                if (subject.toString().equalsIgnoreCase("http://www.semanticweb.org/win10/ontologies/2021/0/untitled-ontology-15#M005")) {
-                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
-                        System.out.print("--------Someone is at Office--------");
-                    }
-                }
+//                //Capteur porte front D001
+//                if (equalToOneCapteursInList(subject,frontDoorCapteurs)){
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        corridor=true;
+//
+//                    }
+//                }
+//                //M31 capteur de mouvement dans BATHROOM
+//                if (equalToOneCapteursInList(subject,BathroomCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        Bathroom=true;
+//
+//                    }
+//                }
+//
+//                //M0014 table
+//                if (equalToOneCapteursInList(subject,tableCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        table=true;
+//
+//
+//                    }
+//                }
+//                //M19 KITCHEN
+//                if (equalToOneCapteursInList(subject,kitchenCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        kitchen=true;
+//
+//                    }
+//                }
+//
+//                //M020 living
+//                if (equalToOneCapteursInList(subject,livingRoomCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        livingRoom=true;
+//
+//                    }
+//                }
+//                //M007 host bedroom
+//                if (equalToOneCapteursInList(subject,hostBedroomCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        hostBedroom=true;
+//
+//                    }
+//                }
+//                //M024 guest bedroom
+//                if (equalToOneCapteursInList(subject,guestBedroomCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        guestBedroom=true;
+//
+//                    }
+//                }
+//                //M027 Office
+//                if (equalToOneCapteursInList(subject,officeCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        guestBedroom=true;
+//
+//                    }
+//                }
+//                //M005 wardrobe
+//                if (equalToOneCapteursInList(subject,wardrobeCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        wardrobe=true;
+//
+//                    }
+//                }
+//
+//                boolean hostToillete=false;
+//                String[] hostToilleteCapteurs=new String[]{"M004"};
+//                if (equalToOneCapteursInList(subject,wardrobeCapteurs)) {
+//                    if ((object.toString().split("http://www")[0]).equalsIgnoreCase("ON^^")) {
+//                        hostToillete=true;
+//
+//                    }
+//                }
 
 
             }
         }
+//        if(frontDoor) System.out.print("-----------someone is opening the front door-----------");
+//        if(Bathroom) System.out.print("-----------someone is using the Bathroom-----------");
+//        if(kitchen) System.out.print("--------Someone is in the kitchen--------");
+//        if(livingRoom) System.out.print("--------Someone is at living room--------");
+//        if(hostBedroom) System.out.print("--------Someone is at host bedroom--------");
+//        if(guestBedroom) System.out.print("--------Someone is at guest bedroom--------");
+//        if(Office) System.out.print("--------Someone is at Office--------");
+//        if(corridor)  System.out.print("---------Someone is the corridor-----------");
+//        if(wardrobe)System.out.print("-------someone is in front of the wardrobe---------");
+//        if(table)  System.out.print("---------Someone is behind the table-----------");
+
+        if(capteurs.get(frontDoorCapteurs)) System.out.print("-----------someone is opening the front door-----------");
+        if(capteurs.get(BathroomCapteurs)) System.out.print("-----------someone is using the Bathroom-----------");
+        if(capteurs.get(kitchenCapteurs)) System.out.print("--------Someone is in the kitchen--------");
+        if(capteurs.get(livingRoomCapteurs)) System.out.print("--------Someone is at living room--------");
+        if(capteurs.get(hostBedroomCapteurs)) System.out.print("--------Someone is at host bedroom--------");
+        if(capteurs.get(guestBedroomCapteurs)) System.out.print("--------Someone is at guest bedroom--------");
+        if(capteurs.get(officeCapteurs)) System.out.print("--------Someone is at Office--------");
+        if(capteurs.get(corridorCapteurs))  System.out.print("---------Someone is in the corridor-----------");
+        if(capteurs.get(wardrobeCapteurs))System.out.print("-------someone is in front of the wardrobe---------");
+        if(capteurs.get(tableCapteurs))  System.out.print("---------Someone is behind the table-----------");
+
+
     }
 }
 
